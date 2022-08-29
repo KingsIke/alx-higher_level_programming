31e6580cea3defac57d4c5fd9d977e2eb16119bb
KINGS=IKE <69237411+KingsIke@users.noreply.github.com>
2022-08-29 22:14:18 +0100

    Python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
-
-
-def main():
-    argc = len(sys.argv)
-    args = sys.argv
-
-    if argc != 4:
-        print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
-        exit(1)
-    else:
-        a = int(args[1])
-        b = int(args[3])
-
-        if args[2] == "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif args[2] == "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif args[2] == "*":
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif args[2] == "/":
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-        else:
-            msg = "Unknown operator. Available operators: +, -, * and /"
-            print("{}".format(msg))
-            exit(1)
-
 
 if __name__ == "__main__":
-    main()
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    if len(argv) != 4:
+        print("Usage:", argv[0], "<a> <operator> <b>")
+        exit(1)
+
+    op = argv[2]
+    f = {"+": add, "-": sub, "*": mul, "/": div}
+    if op not in f:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, f[op](a, b)))
