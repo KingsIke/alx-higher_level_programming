3254c82d5b27e47987f0bfbb41d04bf590a9d14d
KINGS=IKE <69237411+KingsIke@users.noreply.github.com>
2022-08-31 22:23:58 +0100

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    dict_copy = a_dictionary.copy()
-    for x, y in dict_copy.items():
-        if value == y:
-            a_dictionary.pop(x)
-    return a_dictionary
+    for key in list(a_dictionary.keys()):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return(a_dictionary)
